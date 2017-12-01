5c8048ad0aeb2d5a4bec0620f2e9ccd044f6cce2
sachin2smart <sachin2smart@gmail.com>
2017-12-01 23:06:08 +0530

    Signed-off-by: sachin2smart <sachin2smart@gmail.com>

---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+
+//  templateUrl: './app.component.html',
+  template : `
+
+
+    <div style="background:pink;padding-top:100px;padding-bottom:100px;">
+        <h1 style="color:green;text-align:center;">
+            It certainly works !!!
+        </h1>
+    </div>
+
+      <ul>
+        <li *ngFor="let team of teams;
+                    let i=index;
+                    let last=last;
+                    let first=first;
+                    let even=even;
+                    let odd=odd;
+                    ">
+
+              {{i}} . {{team.baseball}}
+              <font color="red">{{first}}<strong *ngIf="first"> First Entry </strong></font>
+              <font color="geen">{{last}}<strong *ngIf="last"> Last Entry </strong></font>
+              <font color="blue">{{even}}<strong *ngIf="even"> Even Entry </strong></font>
+              <font color="magenta">{{odd}}<strong *ngIf="odd"> Odd Entry </strong></font>
+
+        </li>
+      </ul>
+
+  `,
+
+  styleUrls: ['./app.component.css']
+//  styles : [`
+//  h1 {color :brown;}
+//  `
+//  ]
+
+})
+export class AppComponent {
+  title = 'app works !!';
+  teams = [
+              {baseball: 'Indians'},
+              {baseball: 'Mets'},
+              {baseball: 'Yankees'},
+              {baseball: 'Jamees'},
+          ];
+}
+
+/*
+     import { Sanitizer } from '@angular/core';
+
+    @Sanitizer({
+      selector : 'greet',
+      template : '<h2>{{titleForSanitizer}}</h2>',
+      styleUrls : ['./app.component.css']
+    })
+    class greet {
+       titleForSanitizer = 'Sanitizer exported.';
+    }
+*/
+
